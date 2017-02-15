3c6138effd9962e7bc0fefb3eacaec8f7d584e93
luvletter <i@luvletter.moe>
2017-02-15 16:48:56 +0800

    init

---
+++ VenueDialect/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VenueDialect
+//
+//  Created by Luv Letter on 2/15/17.
+//  Copyright © 2017 Luv Letter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
